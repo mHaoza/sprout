f560c2df0ea7e191ce33c968452a0f4c39e59926
haoza <me@iice.fun>
2024-11-09 16:35:21 +0800

    feat: add theme materials

--- src/components/Header/src/index.vue
+++ src/components/Header/src/index.vue
@@ -12,7 +12,7 @@ const navList = [
 </script>
 
 <template>
-  <header class="fixed top-0 left-0 right-0 z-50 flex flex-col justify-center items-center w-screen h-24 transition-[top] bg-[var(--theme-bg-color)] bg-[url('@/assets/textures/frosted.png')]" :class="{ 'slide-down': y > 100 }">
+  <header class="fixed top-0 left-0 right-0 z-50 flex flex-col justify-center items-center w-screen h-24 transition-[top]" :class="{ 'slide-down': y > 100 }">
     <RouterLink to="/" class="header-logo text-2xl mb-2 transition-all">私の</RouterLink>
     <nav class="header-nav">
       <RouterLink v-for="item in navList" :key="item.path" :to="item.path" class="px-2 py-1 mx-3 hover:text-[var(--theme-color)]">{{ item.name }}</RouterLink>
