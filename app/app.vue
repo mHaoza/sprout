7f21349fda7794c960a5c26598740a24ec79ef11
haoza <me@iice.fun>
2025-11-07 09:39:01 +0800

    feat: mobile go yynn.fun

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,19 @@
+<script setup>
+import { useBreakpoints } from '@vueuse/core'
+
+const breakpoints = useBreakpoints({
+  mobile: 768,
+  desktop: Infinity,
+})
+
+// 如果是移动端，就去找u总
+setTimeout(() => {
+  if (breakpoints.smaller('mobile').value) {
+    window.location.href = 'https://www.yynn.fun'
+  }
+}, 200)
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
