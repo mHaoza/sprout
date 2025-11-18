318f6174ab45e8d9c43174c5dc13d81161f0eee3
haoza <me@iice.fun>
2025-11-18 16:45:16 +0800

    fix: banner loading jitter

--- app/components/banner.vue
+++ app/components/banner.vue
@@ -11,16 +11,12 @@ const hasBannerImage = computed(() => !!banner.value?.bannerImage)
 
 <template>
   <div
-    v-if="banner"
     :key="route.path"
-    :class="cn(
-      'banner relative h-100 flex flex-col items-center justify-center text-center',
-      !hasBannerImage && 'h-80',
-    )"
+    :class="cn('banner relative h-100 flex flex-col items-center justify-center text-center')"
   >
     <div v-if="hasBannerImage" class="banner-bg bg-black absolute z-[-1] left-0 top-0 w-full h-full filter-[brightness(.6)]">
       <img
-        :src="banner.bannerImage"
+        :src="banner?.bannerImage"
         loading="lazy"
         :class="cn('w-full h-full object-cover transition-opacity animate-in fade-in duration-500 ease-out')"
       >
@@ -28,14 +24,14 @@ const hasBannerImage = computed(() => !!banner.value?.bannerImage)
 
     <div :class="cn('animate-in fade-in slide-in-from-top-6 duration-600 ease-out text-white', !hasBannerImage && 'text-foreground')">
       <div :class="cn('post-title text-5xl')">
-        <component :is="banner.postTitle" v-if="isVNode(banner.postTitle)" />
+        <component :is="banner?.postTitle" v-if="isVNode(banner?.postTitle)" />
         <div v-else>
-          {{ banner.postTitle }}
+          {{ banner?.postTitle }}
         </div>
       </div>
 
-      <div v-if="banner.postMeta" class="post-meta mt-3">
-        <component :is="banner.postMeta" v-if="isVNode(banner.postMeta)" />
+      <div v-if="banner?.postMeta" class="post-meta mt-3">
+        <component :is="banner?.postMeta" v-if="isVNode(banner.postMeta)" />
         <template v-else>
           {{ banner.postMeta }}
         </template>
