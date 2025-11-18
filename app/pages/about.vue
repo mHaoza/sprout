e0898afb6de8c6cd8d26729dcc46270b9d7d3c72
haoza <me@iice.fun>
2025-11-18 14:57:15 +0800

    refactor: restructure page directory for better organization

---
+++ app/pages/about.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const pageStore = usePageStore()
+
+const meta = {
+  title: '关于',
+  description: '关于我的个人介绍',
+}
+
+onMounted(() => {
+  pageStore.setPageMeta({
+    banner: {
+      postTitle: meta.title,
+      postMeta: 'haoza · 2025-11-11',
+      bannerImage: 'http://127.0.0.1:9000/blog/2025/11/12/0a2de5843667391847c8b0800cc8059b.webp',
+    },
+    description: meta.description,
+  })
+})
+
+useSeoMeta(meta)
+</script>
+
+<template>
+  <div class="">
+    about
+  </div>
+</template>
+
+<style scoped></style>
